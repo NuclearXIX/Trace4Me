9c34c8bf1c00c995ae5747374e72c846c54e31a9
NuclearXIX <bowyer400@gmail.com>
2025-10-22 20:26:42 -0400

    Delete cogs directory

--- cogs/bounty.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-class Bounty(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-# Required setup function
-async def setup(bot):
-    await bot.add_cog(Bounty(bot))
